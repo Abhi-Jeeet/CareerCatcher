eac9d6daf3d5c59fbb04b6ffbe1d63288201c08d
Abhi-Jeeet <abhijeetkumar3245@gmail.com>
2025-06-26 19:52:59 +0530

    Fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,4 +9,19 @@ export default defineConfig({
       '/api': 'http://localhost:5000', // or whatever port your backend runs on
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Vendor chunks
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui-vendor': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          'utils-vendor': ['axios', 'moment', 'react-toastify'],
+          'editor-vendor': ['quill'],
+          'auth-vendor': ['@clerk/clerk-react'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase warning limit to 1MB
+  },
 })
